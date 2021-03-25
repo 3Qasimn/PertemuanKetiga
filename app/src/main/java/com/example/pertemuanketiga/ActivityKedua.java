706c7343b149d5a5b7fed9aaacbbdd0bdfdb1cae
3qasimn <3qasimn@gmail.com>
2021-03-25 19:53:57 +0700

    ActivityKedua

---
+++ app/src/main/java/com/example/pertemuanketiga/ActivityKedua.java
@@ -0,0 +1,32 @@
+package com.example.pertemuanketiga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityKedua extends AppCompatActivity {
+    TextView txEmail, txPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kedua);
+
+        txEmail = findViewById(R.id.emailField);
+        txPassword = findViewById(R.id.passwordField);
+
+        //Mendeclarasikan bundle yang digunakan untuk mengambil pesan
+        //Yang dikirim melalui method intent
+        Bundle bundle = getIntent().getExtras();
+
+        //Membuat variabel string untuk menyimpan data yang dikirim
+        //Dari activity sebelumnya
+        String email = bundle.getString("a");
+        String pass = bundle.getString("b");
+
+        // menampilkan value
+        txEmail.setText(email);
+        txPassword.setText(pass);
+    }
+}
