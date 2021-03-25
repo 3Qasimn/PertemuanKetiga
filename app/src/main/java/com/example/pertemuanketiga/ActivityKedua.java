198454c8da6136c3d5b40cdb6db9fa8a3cc67d70
3qasimn <3qasimn@gmail.com>
2021-03-25 19:56:30 +0700

    PertemuanKeTiga

--- app/src/main/java/com/example/pertemuanketiga/ActivityKedua.java
+++ app/src/main/java/com/example/pertemuanketiga/ActivityKedua.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class ActivityKedua extends AppCompatActivity {
+    //text view
     TextView txEmail, txPassword;
 
     @Override
